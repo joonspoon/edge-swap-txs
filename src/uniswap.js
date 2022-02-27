179a2b243589f7afd4797e77a04241ad1353926f
joonspoon <joonspoon@joonspoon.com>
2022-02-26 17:40:59 -0800

    Added transaction generation for swapping using custom smart contract.

--- src/uniswap.js
+++ src/uniswap.js
@@ -2,35 +2,58 @@ import * as Utils from './utils.js';
 import Web3 from "web3";
 import { Transaction } from 'ethereumjs-tx';
 
+const web3 = new Web3(Utils.getFakeProvider());
+const deadline = Math.floor(Date.now() / 1000) + 60 * 20; // 20 minutes from the current Unix time
+
 export async function generateSwapTransactions(sourceTokenAddress, destinationTokenAddress, amount, senderAddress) {
   // TODO: await generateApprovalIfNecessary()
   const swapTx = await generateSwapTransaction (sourceTokenAddress, destinationTokenAddress, amount, senderAddress);
   return [swapTx];
 }
 
-export async function generateSwapTransaction (sourceTokenAddress, destinationTokenAddress, amount, senderAddress) {
+export async function generateSwapTransactionsForUniswapRouter (swapRequest) {
   const uniswapRouterAddress = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
   const uniswapRouterInterface = Utils.loadContractAbi("uniswap-router");
-
-  const web3 = new Web3(Utils.getFakeProvider());
-  const swapContract = new web3.eth.Contract(uniswapRouterInterface, uniswapRouterAddress);
+  const uniswapContract = new web3.eth.Contract(uniswapRouterInterface, uniswapRouterAddress);
+  const path = [swapRequest.fromCurrencyCode, swapRequest.toCurrencyCode];
 
   /* swapExactETHForTokens(uint amountOutMin, address[] calldata path, address to, uint deadline) */
-  const path = [sourceTokenAddress, destinationTokenAddress];
-  const deadline = Math.floor(Date.now() / 1000) + 60 * 20; // 20 minutes from the current Unix time
-  const swapContractFilled = swapContract.methods.swapExactETHForTokens(0, path, senderAddress, deadline).encodeABI();
-  // const nonce = await web3.eth.getTransactionCount(senderAddress);
+  const swapContractFilled = uniswapContract.methods.swapExactETHForTokens(0, path, swapRequest.fromWallet.address, deadline).encodeABI();
 
   const swapTransaction = {
       chainId: 4, //rinkeby
-      nonce: web3.utils.toHex(107), // <--- manually filling nonce
+      nonce: swapRequest.fromWallet.nonce,
       gasLimit: web3.utils.toHex(500000),
       gasPrice: web3.utils.toHex(10000000000),
-      value: web3.utils.toHex(amount),
+      value: web3.utils.toHex(swapRequest.nativeAmount),
       to: uniswapRouterAddress,
-      from: senderAddress,
+      from: swapRequest.fromWallet.address,
       data: swapContractFilled
   };
 
-  return new Transaction(swapTransaction, { chain: 'rinkeby' })
+  const swapTx = new Transaction(swapTransaction, { chain: 'rinkeby' })
+  return [swapTx];
+}
+
+export async function generateSwapTransactionsForEdgeContract (swapRequest) {
+  const edgeContractAddress = "0x84F4d73e9D679fc487cC819f02069096b4aBE210";
+  const edgeContractInterface = Utils.loadContractAbi("edge-swap");
+  const swapContract = new web3.eth.Contract(edgeContractInterface, edgeContractAddress);
+
+  /* function swapFromETH(address _tokenOut, uint _amountOutMin) */
+  const swapContractFilled = swapContract.methods.swapFromETH(swapRequest.toCurrencyCode, 0).encodeABI();
+
+  const swapTransaction = {
+      chainId: 4, //rinkeby
+      nonce: swapRequest.fromWallet.nonce,
+      gasLimit: web3.utils.toHex(500000),
+      gasPrice: web3.utils.toHex(10000000000),
+      value: web3.utils.toHex(swapRequest.nativeAmount),
+      to: edgeContractAddress,
+      from: swapRequest.fromWallet.address,
+      data: swapContractFilled
+  };
+
+  const swapTx = new Transaction(swapTransaction, { chain: 'rinkeby' })
+  return [swapTx];
 }
--- testSwap.js
+++ testSwap.js
@@ -1,17 +1,31 @@
 import { ethers } from "ethers";
 import * as Uniswap from './src/uniswap.js';
 import * as Utils from './src/utils.js'
-import Web3 from "web3";
 
+/* ------------ user settings ------------ */
 const sourceAsset = 'WETH';
 const destinationAsset= 'UNI';
 const amountToSwap = ethers.utils.parseEther(".001");
+const useEdgeContract = true;
 const walletAddress = "0x0c74caFf031C9cefbBCdF84Ed5363f928078DA51";
+/* ------------ user settings ------------ */
 
+const walletNonce = await Utils.getNonceForWallet(walletAddress);
 const sourceTokenAddress = Utils.getRinkebyAddressFromCurrencyCode(sourceAsset);
 const destinationTokenAddress = Utils.getRinkebyAddressFromCurrencyCode(destinationAsset);
 
-const transactions = await Uniswap.generateSwapTransactions(sourceTokenAddress, destinationTokenAddress, amountToSwap, walletAddress);
+const swapRequest = {
+  fromWallet: { address: walletAddress, nonce: walletNonce },
+  fromCurrencyCode: sourceTokenAddress,
+  toCurrencyCode: destinationTokenAddress,
+  nativeAmount: amountToSwap,
+}
+
+let transactions = [];
+if (useEdgeContract)
+   transactions = await Uniswap.generateSwapTransactionsForEdgeContract(swapRequest);
+else
+   transactions = await Uniswap.generateSwapTransactionsForUniswapRouter(swapRequest);
 
 for (const transaction of transactions) {
   const result = await Utils.signAndSendTransaction(transaction);
