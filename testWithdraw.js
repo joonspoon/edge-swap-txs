5e1f3e8372f1b935acc9c730e34420dc082ea107
joonspoon <joonspoon@joonspoon.com>
2022-03-01 15:21:57 -0800

    Added code to withdraw fees from the contract.

---
+++ testWithdraw.js
@@ -0,0 +1,31 @@
+import { ethers } from "ethers";
+import * as Utils from './src/utils.js';
+
+import { createRequire } from "module"; // Bring in the ability to create the 'require' method
+const require = createRequire(import.meta.url); // construct the require method
+const secrets = require('./src/secrets.json') // use the require method
+
+const proxyContractAddress = '0xAd14652864994d93FeDb1B7f59337372C453E6BD'; //Rinkeby testnet
+const proxyContractInterface = [ "function withdrawFees() external" ];
+
+const rinkebyProvider = ethers.providers.getDefaultProvider('rinkeby');
+const ethersWallet = new ethers.Wallet(secrets.privateKey, rinkebyProvider);
+const signedContract = new ethers.Contract(proxyContractAddress, proxyContractInterface, ethersWallet);
+
+var gasOptions = { gasPrice: 1000000000, gasLimit: 1000000};
+
+const result = await signedContract.withdrawFees(gasOptions);
+const { hash } = result;
+console.log( "https://rinkeby.etherscan.io/tx/" + hash);
+
+
+// const proxyContractInterface = [ "function withdrawExoticFees(address _token, address _recipient) external" ];
+// const uniAddress = "0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984";
+// const walletAddress = "0x0c74caFf031C9cefbBCdF84Ed5363f928078DA51";
+// var gasOptions = { gasPrice: 1000000000, gasLimit: 100000};
+// const rinkebyProvider = ethers.providers.getDefaultProvider('rinkeby');
+// const ethersWallet = new ethers.Wallet(secrets.ownerPrivateKey, rinkebyProvider);
+// const signedContract = new ethers.Contract(proxyContractAddress, proxyContractInterface, ethersWallet);
+// const result = await signedContract.withdrawExoticFees(uniAddress, walletAddress, gasOptions);
+// const { hash } = result;
+// console.log( "https://rinkeby.etherscan.io/tx/" + hash);
