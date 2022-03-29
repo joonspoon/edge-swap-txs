f433047725865dfe4e631c78859c0013991d3462
joonspoon <joonspoon@joonspoon.com>
2022-03-29 12:18:00 -0700

    Removed code not needed for checking liquidity.

--- testCheckLiquidity.js
+++ testCheckLiquidity.js
@@ -6,16 +6,12 @@ import * as Utils from './src/utils.js'
 const sourceAsset = 'DAI';
 const destinationAsset= 'WETH';
 const amountToSwap = 1;
-const useEdgeContract = true;
-const walletAddress = "0x0c74caFf031C9cefbBCdF84Ed5363f928078DA51";
 /* ------------ user settings ------------ */
 
-const walletNonce = await Utils.getNonceForWallet(walletAddress);
 const sourceTokenAddress = Utils.getRinkebyAddressFromCurrencyCode(sourceAsset);
 const destinationTokenAddress = Utils.getRinkebyAddressFromCurrencyCode(destinationAsset);
 
 const swapRequest = {
-  fromWallet: { address: walletAddress, nonce: walletNonce },
   fromCurrencyCode: sourceTokenAddress,
   // toCurrencyCode: "0x58c2E9C13AC84DFD5a4c522DC55ab5e1829319D6", // scam token that can be used to test for no liquidity
   toCurrencyCode: destinationTokenAddress,
